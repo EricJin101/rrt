ed9cb739ea6c9032ebfdff8fb39b889096d6b6f9
EricJin101 <foreveryouth93@163.com>
2021-04-08 18:00:31 +0800

    rough structure

--- rrt/rrt_algo.cc
+++ rrt/rrt_algo.cc
@@ -2,3 +2,36 @@
 //
 
 #include "rrt_algo.h"
+namespace eric{
+    namespace rrt_algo{
+    void rrtNear(PathPoint *point, Path *global_map)
+    {
+        /**
+         * @brief: sampling from map
+         * @param: point is current position,
+         *         global_map is planned path
+         * */
+
+    }
+    void rrtStepForward(PathPoint *p_rand, double stepSize)
+    {
+        /** @brief:
+         *  @param:
+         * */
+    }
+    bool rrtFinishCondition(PathPoint *p_new, PathPoint *p_goal)
+    {
+        if (p_new == p_goal)
+        {
+            return true;
+        }
+        return false;
+    }
+    void rrtCollisionDetect()
+    {
+        /**
+         * @brief:
+         * */
+    }
+    }
+}
--- rrt/rrt_algo.h
+++ rrt/rrt_algo.h
@@ -3,11 +3,29 @@
 
 #ifndef RRT_RRT_ALGO_H
 #define RRT_RRT_ALGO_H
+#include "vector"
+namespace eric{
+namespace rrt_algo{
+    struct PathPoint {
+        double x;
+        double y;
+        double theta;
+    };
+    struct Path : PathPoint{
+        vector<PathPoint> globalMap;
+    };
+    bool operator==(const PathPoint p1, const PathPoint p2)
+    {
+        if (p1.x == p2.x && p1.y == p2.y ) return true;
+    }
+    class rrt_algo {
+        // map
+        // find near
+        // new
+    };
+}
+}
 
-
-class rrt_algo {
-
-};
 
 
 #endif //RRT_RRT_ALGO_H
